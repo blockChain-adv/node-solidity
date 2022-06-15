edacbd1d76922317dc4b534edfe6deb6ef88e8bf
97Kzone <76652908+97Kzone@users.noreply.github.com>
2022-06-15 21:51:39 +0900

    Feat. 광고 구조 변경, 에러 핸들러 변경 광고 구조 flag 추가 = 만료된 광고 확인

--- contracts/Advertising.sol
+++ contracts/Advertising.sol
@@ -13,8 +13,9 @@ contract Advertising {
     struct AdvInfo {
         string title; // 광고제목 
         string comment; // 광고내용
+        
+        bool flag; // 광고 만료되면 False
         uint256 pot; // 광고비
-        uint256 count; // 목표 클릭 수.. 인데 금액이 정확하게 1/N 이 안된다면 어쩌지?
         uint256 reward; // 1회 시청 시 리워드 금액
     }
 
@@ -28,8 +29,7 @@ contract Advertising {
     
     //광고 이벤트
     event AddAdv(address addr, string _title, uint256 _pot, uint256 _count); // 광고 추가되면 이벤트 발생시켜서 내역 확인
-    event howmuch(uint256 val); // 송금 확인용 이벤트
-    event test(AdvInfo info);
+    event IsCall(address addr, uint256 _rwd); // 송금 확인용 이벤트
 
     // 광고 등록
     function addAdv(string memory _title, string memory _comment, uint256 _pot, uint256 _count) public payable returns (bool res) {
@@ -40,8 +40,8 @@ contract Advertising {
         a.title = _title;
         a.comment = _comment;
         a.pot = _pot;
-        a.count = _count;
         a.reward = _pot / _count;
+        a.flag = true;
 
         if (_advs[msg.sender].length == 0) // 처음 광고 등록하면 광고주 명단에 박제
         { 
@@ -76,22 +76,23 @@ contract Advertising {
     // 광고 시청 시 리워드 지급
     function callReward(uint256 idx) public returns(string memory) {
         AdvInfo memory a = AllAdv[idx];
-        uint256 _cnt = a.count;
         uint256 _rwd = a.reward;
         uint256 _pot = a.pot;
         address payable addr = payable(msg.sender);
 
-        require(_cnt > 1, "Expired advertising");
+        // 광고비가 지급할 리워드 만큼 남아있는가 ?
+        if (_pot < _rwd) {
+            AllAdv[idx].flag = false; // 만료된 광고로 전환
+            revert("Expired advertising"); // 그리고 Error 발생, Gas 회수
+        }
 
         (bool sent, ) = addr.call{value: _rwd}("");
 
-        require(sent, "Fail to transfer reward");
+        require(sent, "Fail to transfer reward"); // 송금이 잘 됐는가?
         AllAdv[idx].pot = _pot - _rwd;
 
-        emit test(AllAdv[idx]);
+        emit IsCall(msg.sender, _rwd);
 
         return "Reward Completed";
     }
-
-    // 등록된 광고는 수정이 가능해야 할까 ?
 }
